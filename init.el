8dd2507578b014c42d67a0d6e0c0640f2f8681a2
Yury Kholodkov <qeqpep@autistici.org>
2024-03-26 10:09:11 +0300

    Themes, keybindings and switcb to sturdy.

--- init.el
+++ init.el
@@ -3,20 +3,22 @@
 ;; Why do you need a macro that defines anonymous function so often? What's the deal with Named Builder?
 ;; (setup  (:p emit :host github :repo "IvanMalison/emit"))
 ;;; Typography
-;;;; Nano theme
-;; Modus are nice, but semantic information is more important for me than just accessibility.
-;; TODO: context!!
-(setup (:p nano-theme) (:require nano-theme)
-       ;; TODO: nano support fontaine/defaults 
-       (set-face-attribute 'nano-mono t :family "Iosevka Comfy")
-       (set-face-attribute 'nano-sans t :family "Iosevka Comfy Duo")
-       (set-face-attribute 'nano-serif t :family "Iosevka Comfy Motion Duo")
-       (set-face-attribute 'nano-italic t :family "Iosevka Comfy Motion")
-       (nano-mode))
+;;;; Modus themes
+(setup (:p modus-themes)
+  (modus-themes-select 'modus-operandi))
 
+;;;; Fontaine
+(setup (:p fontaine)
+  (setopt fontaine-presets '((small :default-height 120)
+                             (regular :default-height 140)
+                             (t :default-family "Iosevka Comfy Duo")))
+  (fontaine-mode)
+  (fontaine-set-preset 'regular))
+
+;;;; Auto dark
 (setup (:p auto-dark)
-  (setopt auto-dark-dark-theme 'nano-dark
-          auto-dark-light-theme 'nano-light)
+  (setopt auto-dark-dark-theme 'modus-vivendi
+          auto-dark-light-theme 'modus-operandi)
   (auto-dark-mode))
   
 ;;;; Outli
@@ -64,16 +66,104 @@
 ;;;; Guix
 ;; TODO integrate containers with tramp, etc.
 
-;;;; Modal/Structural editing
+;;; Keyboarding
 ;; The dream is to take this mostly out of emacs with https://github.com/shegeley/haits
 
 ;; (setup (:p symex :host github :repo "drym-org/symex.el" :branch "2.0-integration")
 ;;   (symex-initialize))
 
-(setup (:p meow))
+;;;; Meow
+(setup (:p meow)
+  (require 'meow)
+  (defun my-meow-setup ()
+     (meow-motion-overwrite-define-key
+      '("n" . meow-next)
+      '("a" . meow-prev)
+      '("<escape>" . ignore))
+     (meow-leader-define-key
+      ;; SPC j/k will run the original command in MOTION state.
+      '("n" . "H-n")
+      '("a" . "H-a")
+      ;; Use SPC (0-9) for digit arguments.
+      '("1" . meow-digit-argument)
+      '("2" . meow-digit-argument)
+      '("3" . meow-digit-argument)
+      '("4" . meow-digit-argument)
+      '("5" . meow-digit-argument)
+      '("6" . meow-digit-argument)
+      '("7" . meow-digit-argument)
+      '("8" . meow-digit-argument)
+      '("9" . meow-digit-argument)
+      '("0" . meow-digit-argument)
+      '("/" . meow-keypad-describe-key)
+      '("?" . meow-cheatsheet))
+     (meow-normal-define-key
+      '("0" . meow-expand-0)
+      '("9" . meow-expand-9)
+      '("8" . meow-expand-8)
+      '("7" . meow-expand-7)
+      '("6" . meow-expand-6)
+      '("5" . meow-expand-5)
+      '("4" . meow-expand-4)
+      '("3" . meow-expand-3)
+      '("2" . meow-expand-2)
+      '("1" . meow-expand-1)
+      '("-" . negative-argument)
+      '("i" . meow-reverse)
+      '("'" . meow-inner-of-thing)
+      '(";" . meow-bounds-of-thing)
+      '("[" . meow-beginning-of-thing)
+      '("]" . meow-end-of-thing)
+      '("p" . meow-append)
+      '("P" . meow-open-below)
+      '("b" . meow-back-word)
+      '("B" . meow-back-symbol)
+      '("c" . meow-change)
+      '("d" . meow-delete)
+      '("D" . meow-backward-delete)
+      '("e" . meow-next-word)
+      '("E" . meow-next-symbol)
+      '("f" . meow-find)
+      '("g" . meow-cancel-selection)
+      '("G" . meow-grab)
+      '("." . meow-left)
+      '(">" . meow-left-expand)
+      '("i" . meow-insert)
+      '("I" . meow-open-above)
+      '("n" . meow-next)
+      '("N" . meow-next-expand)
+      '("a" . meow-prev)
+      '("A" . meow-prev-expand)
+      '("l" . meow-right)
+      '("L" . meow-right-expand)
+      '("m" . meow-join)
+      '("h" . meow-search)
+      '("o" . meow-block)
+      '("O" . meow-to-block)
+      '("j" . meow-yank)
+      '("q" . meow-quit)
+      '("Q" . meow-goto-line)
+      '("r" . meow-replace)
+      '("R" . meow-swap-grab)
+      '("s" . meow-kill)
+      '("t" . meow-till)
+      '("u" . meow-undo)
+      '("U" . meow-undo-in-selection)
+      '("v" . meow-visit)
+      '("w" . meow-mark-word)
+      '("W" . meow-mark-symbol)
+      '("x" . meow-line)
+      '("X" . meow-goto-line)
+      '("y" . meow-save)
+      '("Y" . meow-sync-grab)
+      '("z" . meow-pop-selection)
+      '("/" . repeat)
+      '("<escape>" . ignore)))
+  (my-meow-setup)
 
-(setup (:p parinfer-rust-mode :host github :repo "justinbarclay/parinfer-rust-mode")
-  (:hook-into lisp-mode scheme-mode emacs-lisp-mode))
+  (meow-global-mode t))
+ 
+
 ;;; Language
 ;;;; Rust
 ;; (use-package rustic
@@ -81,7 +171,18 @@
 ;;   (rustic-lsp-client 'eglot)
 ;;   :hook
 ;;   (eglot--managed-mode-hook . (lambda () (flymake-mode -1))))
+;;;; Lisp
+;;;;; Arei
+(setup (:p arei :host sourcehut :repo "abcdw/emacs-arei"))
+
+;;;;; Parinfer
+(setup (:p parinfer-rust-mode :host github :repo "justinbarclay/parinfer-rust-mode")
+  (:hook-into lisp-mode scheme-mode emacs-lisp-mode))
+
 ;;; Files
+;;;; Save place
+(save-place-mode)
+
 ;;; Completion
 (setup (:p vertico)
   (vertico-mode))
@@ -115,11 +216,10 @@
 ;; golly those long-running commands, hide them away from me, or I'd spend all my waking life waiting for them to complete (guix...)
 
 ;;; WinSanity
-;;;; Save place
-(save-place-mode)
-  
 ;;;; Undo
 (winner-mode)
+;;;; Activities
+(setup (:p activities :local-repo "~/s/e/activities")) 
 
 ;;;; Burly
 (setup (:p burly :host github :repo "alphapapa/burly.el")
